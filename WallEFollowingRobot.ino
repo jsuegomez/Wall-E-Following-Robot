02306000e0aefb4fbcd5eb746e699b8d6e82b8bf
jsuegomez <114530026+jsuegomez@users.noreply.github.com>
2025-11-25 12:05:18 -0500

    Update WallEFollowingRobot.ino

--- WallEFollowingRobot.ino
+++ WallEFollowingRobot.ino
@@ -1,7 +1,7 @@
 #include <NewPing.h>
 
-#define ULTRASONIC_SENSOR_TRIG 11
-#define ULTRASONIC_SENSOR_ECHO 12
+#define ULTRASONIC_SENSOR_TRIG 5
+#define ULTRASONIC_SENSOR_ECHO 4
 
 #define MAX_FORWARD_MOTOR_SPEED 75
 #define MAX_MOTOR_TURN_SPEED_ADJUSTMENT 50
@@ -9,102 +9,110 @@
 #define MIN_DISTANCE 10
 #define MAX_DISTANCE 30
 
-#define IR_SENSOR_RIGHT 2
-#define IR_SENSOR_LEFT 3
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 10
 
-//Right motor
-int enableRightMotor=5;
-int rightMotorPin1=7;
-int rightMotorPin2=8;
+// Right motor: connect to driver IN1 / IN2
+int rightMotorPin1 = 7;
+int rightMotorPin2 = 6;
 
-//Left motor
-int enableLeftMotor=6;
-int leftMotorPin1=9;
-int leftMotorPin2=10;
+// Left motor: connect to driver IN3 / IN4
+int leftMotorPin1 = 3;
+int leftMotorPin2 = 2;
 
+// Ultrasonic sensor (max distance 400 cm)
 NewPing mySensor(ULTRASONIC_SENSOR_TRIG, ULTRASONIC_SENSOR_ECHO, 400);
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(enableRightMotor, OUTPUT);
+  // Motor pins as outputs
   pinMode(rightMotorPin1, OUTPUT);
   pinMode(rightMotorPin2, OUTPUT);
-  
-  pinMode(enableLeftMotor, OUTPUT);
-  pinMode(leftMotorPin1, OUTPUT);
-  pinMode(leftMotorPin2, OUTPUT);
+  pinMode(leftMotorPin1,  OUTPUT);
+  pinMode(leftMotorPin2,  OUTPUT);
 
+  // IR sensors as inputs
   pinMode(IR_SENSOR_RIGHT, INPUT);
-  pinMode(IR_SENSOR_LEFT, INPUT);
-  rotateMotor(0,0);   
-}
+  pinMode(IR_SENSOR_LEFT,  INPUT);
 
+  // Make sure motors are stopped at startup
+  stopMotors();
+}
 
 void loop()
 {
-  int distance = mySensor.ping_cm();
+  int distance          = mySensor.ping_cm();
   int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+  int leftIRSensorValue  = digitalRead(IR_SENSOR_LEFT);
 
-  //NOTE: If IR sensor detects the hand then its value will be LOW else the value will be HIGH
-  
-  //If right sensor detects hand, then turn right. We increase left motor speed and decrease the right motor speed to turn towards right
-  if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  // NOTE: If IR sensor detects the hand its value will be LOW,
+  // otherwise the value will be HIGH.
+
+  // If right sensor detects hand, then turn right
+  if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH)
   {
-      rotateMotor(MAX_FORWARD_MOTOR_SPEED - MAX_MOTOR_TURN_SPEED_ADJUSTMENT, MAX_FORWARD_MOTOR_SPEED + MAX_MOTOR_TURN_SPEED_ADJUSTMENT ); 
+    turnRight();
   }
-  //If left sensor detects hand, then turn left. We increase right motor speed and decrease the left motor speed to turn towards left
-  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  // If left sensor detects hand, then turn left
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW)
   {
-      rotateMotor(MAX_FORWARD_MOTOR_SPEED + MAX_MOTOR_TURN_SPEED_ADJUSTMENT, MAX_FORWARD_MOTOR_SPEED - MAX_MOTOR_TURN_SPEED_ADJUSTMENT); 
+    turnLeft();
   }
-  //If distance is between min and max then go straight
+  // If distance is between min and max then go straight
   else if (distance >= MIN_DISTANCE && distance <= MAX_DISTANCE)
   {
-    rotateMotor(MAX_FORWARD_MOTOR_SPEED, MAX_FORWARD_MOTOR_SPEED);
+    moveForward();
   }
-  //stop the motors
-  else 
+  // Otherwise stop the motors
+  else
   {
-    rotateMotor(0, 0);
+    stopMotors();
   }
 }
 
+// ===== Motor helper functions (no enable pins) =====
 
-void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+// Both motors forward
+void moveForward()
 {
-  if (rightMotorSpeed < 0)
-  {
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,HIGH);    
-  }
-  else if (rightMotorSpeed > 0)
-  {
-    digitalWrite(rightMotorPin1,HIGH);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
-  else
-  {
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
+  // Right motor forward
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
 
-  if (leftMotorSpeed < 0)
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,HIGH);    
-  }
-  else if (leftMotorSpeed > 0)
-  {
-    digitalWrite(leftMotorPin1,HIGH);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-  else 
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-  analogWrite(enableRightMotor, abs(rightMotorSpeed));
-  analogWrite(enableLeftMotor, abs(leftMotorSpeed));    
+  // Left motor forward
+  digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+}
+
+// Turn right: left motor forward, right motor stopped
+void turnRight()
+{
+  // Right motor stopped
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, LOW);
+
+  // Left motor forward
+  digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+}
+
+// Turn left: right motor forward, left motor stopped
+void turnLeft()
+{
+  // Left motor stopped
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, LOW);
+
+  // Right motor forward
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+}
+
+// Stop both motors
+void stopMotors()
+{
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, LOW);
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, LOW);
 }
