724d94a1def006eca343280bca62a478c028c977
jsuegomez <114530026+jsuegomez@users.noreply.github.com>
2025-12-01 14:26:20 -0500

    Added head servo motor motion

--- WallEFollowingRobot.ino
+++ WallEFollowingRobot.ino
@@ -1,4 +1,5 @@
 #include <NewPing.h>
+#include <Servo.h>
 
 #define ULTRASONIC_SENSOR_TRIG 5
 #define ULTRASONIC_SENSOR_ECHO 4
@@ -12,6 +13,8 @@
 #define IR_SENSOR_RIGHT 11
 #define IR_SENSOR_LEFT 10
 
+#define servoPin = 13
+
 // Right motor: connect to driver IN1 / IN2
 int rightMotorPin1 = 7;
 int rightMotorPin2 = 6;
@@ -19,6 +22,10 @@ int rightMotorPin2 = 6;
 // Left motor: connect to driver IN3 / IN4
 int leftMotorPin1 = 3;
 int leftMotorPin2 = 2;
+
+Servo headServo;
+int servoAngle = 0:
+int servoDirection = 1:
 
 // Ultrasonic sensor (max distance 400 cm)
 NewPing mySensor(ULTRASONIC_SENSOR_TRIG, ULTRASONIC_SENSOR_ECHO, 400);
@@ -35,13 +42,16 @@ void setup()
   pinMode(IR_SENSOR_RIGHT, INPUT);
   pinMode(IR_SENSOR_LEFT,  INPUT);
 
+  headServo.attach(servoPin);
+  headServo.write(90);
+
   // Make sure motors are stopped at startup
   stopMotors();
 }
 
 void loop()
 {
-  int distance          = mySensor.ping_cm();
+  int distance = mySensor.ping_cm();
   int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
   int leftIRSensorValue  = digitalRead(IR_SENSOR_LEFT);
 
@@ -52,16 +62,19 @@ void loop()
   if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH)
   {
     turnRight();
+    moveHead();
   }
   // If left sensor detects hand, then turn left
   else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW)
   {
     turnLeft();
+    moveHead();
   }
   // If distance is between min and max then go straight
   else if (distance >= MIN_DISTANCE && distance <= MAX_DISTANCE)
   {
     moveForward();
+    moveHead();
   }
   // Otherwise stop the motors
   else
@@ -115,4 +128,22 @@ void stopMotors()
   digitalWrite(rightMotorPin2, LOW);
   digitalWrite(leftMotorPin1, LOW);
   digitalWrite(leftMotorPin2, LOW);
+}
+
+// Move the head by using the servo motor
+void moveHead()
+{
+    servoAngle += servoDirection;
+
+  if (servoAngle >= 140) {
+    servoAngle = 140;
+    servoDirection = -1;
+  }
+  else if (servoAngle <= 40) {
+    servoAngle = 40;
+    servoDirection = 1;
+  }
+
+  headServo.write(servoAngle);
+  delay(10); 
 }
